c7838922f0bf0f054874a3360614eca2337f8769
Mario Krajacic <krajacic.mario@gmail.com>
2023-04-05 11:56:20 +0200

    Initial Commit

---
+++ I Am Rich with SwiftUI/I_Am_Rich_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_with_SwiftUIApp.swift
+//  I Am Rich with SwiftUI
+//
+//  Created by Mario Krajačić on 05.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
